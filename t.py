6149f7087ca6ebffde83c20beaef60341f22bda6
Ömer Günal <o.gunal977@gmail.com>
2018-02-17 22:59:16 +0300

    Create t.py

---
+++ t.py
@@ -0,0 +1,95 @@
+import tweepy
+import re
+import os
+import sys
+import argparse
+from collections import Counter
+
+consumer_key = "x"
+consumer_secret = "x"
+access_key = "x-x"
+access_secret = "x"
+
+class Fake():
+# fake human class
+	def __init__(self, user):
+		self.username = user.screen_name
+		self.name = user.name
+		self.description = user.description
+		self.tweetCount = str(user.statuses_count)
+		self.following = str(user.friends_count)
+		self.followers = str(user.followers_count)
+		self.favs = str(user.favourites_count)
+		self.website = str(user.url)
+		self.created = str(user.created_at)
+		self.location = user.location
+		self.lang = user.lang
+		self.profilePic = "https://twitter.com/"+user.screen_name+"/profile_image?size=original"
+		self.banner = user.profile_banner_url
+
+	def write(self):
+		print(self.username)
+		print(self.name)
+		print(self.description)
+		print(self.tweetCount)
+		print(self.following)
+		print(self.banner)
+
+	def spoofProfile(self, api):
+		# update fake profile from original
+		os.system("wget -O profile.jpg "+ "https://twitter.com/"+self.username+"/profile_image?size=original")
+		os.system("wget -O banner.jpg " + self.banner)
+		api.update_profile(self.name, "", self.location, self.description)
+		api.update_profile_image("profile.jpg")
+		api.update_profile_banner("banner.jpg")
+
+	def getMentions(self, api):
+		allTweets = ""
+		mentionList = []
+		public_tweets = api.user_timeline(screen_name = self.username,count = 500)
+
+		for tweet in public_tweets: # get tweets
+			allTweets += tweet.text
+
+		for mention in allTweets.split(" "):
+			if mention[:1] == "@": #filter mentions
+                               mentionList.append(mention)
+		countMention = Counter(mentionList)
+		print("Best accounts for deceiving the target")
+		print("***********************")
+		print(countMention.most_common(5))
+		print("***********************")
+		print(allTweets)
+
+	def getTweets(self, api):
+		allTweets = ""
+		public_tweets = api.user_timeline(screen_name = self.username,count = 500)
+		for tweet in public_tweets: # get tweets
+			tweet.text = re.sub(r"http\S+", "", tweet.text) # remove URLs
+			tweet.text = re.sub(r"RT", "", tweet.text) # remote "RT" text
+			tweet.text = re.sub(r'[\^@*][^\W]*', '', tweet.text) # remove @
+
+			print(tweet.text)
+			allTweets += tweet.text
+
+def main():
+	try:
+		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+		auth.set_access_token(access_key, access_secret)
+		auth.get_authorization_url()
+		api = tweepy.API(auth)
+	except tweepy.TweepError:
+		print ('Hata')
+
+	username = sys.argv[1]
+	user = api.get_user(username)
+
+	test = Fake(user)
+	#test.write()
+	#test.spoofProfile(api)
+	#test.getMentions(api)
+	test.getTweets(api)
+
+
+if __name__ == "__main__":
+	main()
