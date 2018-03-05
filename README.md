ba461c995e9642c789219cdea2442535ff2180fa
Aidan Holland <thehappydinoa@gmail.com>
2018-03-04 19:17:05 -0500

    Updated Style and Functions
    - Simplified Code
    - Changed `wget` to `urlretrieve`
    - Added Shebang
    - Added argparse
    - Added configparse

--- README.md
+++ README.md
@@ -1,31 +1,35 @@
 # Phishing on Twitter
+
 ## Generate tweet automatically like him/her
-![graph1](https://github.com/omergunal/T/blob/master/img/12.png)
-![t3](https://github.com/omergunal/PoT/blob/master/img/t3.png)
+
+![graph1](https://github.com/omergunal/T/blob/master/img/12.png) ![t3](https://github.com/omergunal/PoT/blob/master/img/t3.png)
+
 ### How it works?
 
-* 1- Collect data from target's twitter account
-* 2- Find target's friend and copy her/him account
-* 3- Generate tweet automatically with markov chain algorithm and send it
+- 1- Collect data from target's twitter account
+- 2- Find target's friend and copy her/him account
+- 3- Generate tweet automatically with markov chain algorithm and send it
 
 ### Installation
-```
+
+```bash
 git clone https://github.com/omergunal/PoT
 cd PoT
 pip3 install -r requirements.txt
 ```
-Update your api keys in "PoT.py". Go to https://apps.twitter.com/ and get API keys
+
+Update your api keys in "PoT.cfg". Go to <https://apps.twitter.com/> and get API keys
 
 ### Usage
-```
-python3 PoT.py [target_account_name]
+
+```bash
+python3 PoT.py -u username
 ```
 
 ### ScreenShots
 
-![t1](https://github.com/omergunal/PoT/blob/master/img/t1.png)
-![t2](https://github.com/omergunal/PoT/blob/master/img/t2.png)
-![t3](https://github.com/omergunal/PoT/blob/master/img/t3.png)
+![t1](https://github.com/omergunal/PoT/blob/master/img/t1.png) ![t2](https://github.com/omergunal/PoT/blob/master/img/t2.png) ![t3](https://github.com/omergunal/PoT/blob/master/img/t3.png)
 
 # !
+
 Don't use for illegal activity
--- tweetGenerator.py
+++ tweetGenerator.py
@@ -1,49 +1,30 @@
-#MIT License
-
-#Copyright (c) 2018 Ömer Günal
-
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
+#!/usr/bin/env python3
 
 import random
 
 order = 4
 ngrams = {}
 
+
 class TweetGenerator():
-#tweet generator with markov chain algorithm
+    # tweet generator with markov chain algorithm
     def __init__(self, txt):
-            self.txt = txt
+        self.txt = txt
 
     def setup(self, txt):
-        for i in range(0,len(txt) - order):
-            gram = txt[i:i+order]
+        for i in range(0, len(txt) - order):
+            gram = txt[i:i + order]
             if gram not in ngrams:
                 ngrams[gram] = []
-            ngrams[gram].append(txt[i+order])
+            ngrams[gram].append(txt[i + order])
         currentGram = txt[0:order]
         result = currentGram
 
-        for i in range(0,75): #
+        for i in range(0, 75):
             possibilities = ngrams[currentGram]
             if(not possibilities):
                 break
             next = random.choice(possibilities)
             result += next
-            currentGram = result[len(result)-order:len(result)]
+            currentGram = result[len(result) - order:len(result)]
         return(result)
